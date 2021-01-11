0984897f0d68c3a0503d531ef940ffc89aec0344
gtg7784 <tae.gun7784@gmail.com>
2021-01-11 21:04:16 +0900

    Add Weather Service

--- app/src/main/java/co/gtg7784/retrofitjavatutorial/WeatherService.java
+++ app/src/main/java/co/gtg7784/retrofitjavatutorial/WeatherService.java
@@ -1,4 +1,13 @@
 package co.gtg7784.retrofitjavatutorial;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface WeatherService {
+    @GET("/data/2.5/weather")
+    Call<WeatherResponse> getWeatherData(
+            @Query("q") String city,
+            @Query("appid") String appId
+    );
 }
