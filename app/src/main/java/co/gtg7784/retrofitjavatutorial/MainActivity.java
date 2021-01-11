1c576d1d7390ed7e4b22d7fbcdb82fad4efd6d78
gtg7784 <tae.gun7784@gmail.com>
2021-01-11 20:17:59 +0900

    Create Android Java Project with Empty Activity

---
+++ app/src/main/java/co/gtg7784/retrofitjavatutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package co.gtg7784.retrofitjavatutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
