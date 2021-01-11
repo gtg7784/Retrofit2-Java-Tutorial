77c0463c17638f899fd55f52dba67fe12d601082
gtg7784 <tae.gun7784@gmail.com>
2021-01-11 21:09:01 +0900

    Update MainActivity.java

--- app/src/main/java/co/gtg7784/retrofitjavatutorial/MainActivity.java
+++ app/src/main/java/co/gtg7784/retrofitjavatutorial/MainActivity.java
@@ -3,12 +3,66 @@ package co.gtg7784.retrofitjavatutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    static final String baseURL = "https://api.openweathermap.org/";
+    static final String appId = "";
+    static final String city = "Seoul";
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        WeatherService service = retrofit.create(WeatherService.class);
+
+        Call<WeatherResponse> call = service.getWeatherData(city, appId);
+
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                WeatherResponse body = response.body();
+                String weather = body.weather.get(0).main;
+                String description = body.weather.get(0).description;
+                double temp = body.main.temp - 273.15;
+
+
+                String result = "도시 : " + city +
+                    "\n날씨 : " + weather +
+                    "\n날씨 상세 : " + description +
+                    "\n온도 : " + temp;
+
+                text.setText(result);
+            }
+
+            @Override
+            public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                Log.d("MainActivy", "error : " + t.getMessage());
+            }
+        });
     }
 }
